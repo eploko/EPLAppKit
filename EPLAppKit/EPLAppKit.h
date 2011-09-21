d0ca4d9603183e277a48dd89b9df0620d00a023a
Andrey Subbotin <andrey@subbotin.me>
2011-09-22 00:30:41 +0400

    Initial Commit

---
+++ EPLAppKit/EPLAppKit.h
@@ -0,0 +1,13 @@
+//
+//  EPLAppKit.h
+//  EPLAppKit
+//
+//  Created by Andrey Subbotin on 9/22/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface EPLAppKit : NSObject
+
+@end
